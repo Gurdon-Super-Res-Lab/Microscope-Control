545438e86b81f0dc3fe07400675aafb489d93f0a
Edward <ea436@cam.ac.uk>
2021-07-29 12:18:40 +0100

    Continuous buffer read for larger file sizes. Have confirmed it works for 100MB TIF stack.

--- Tools/TCP Send Image/Example_TCP_Recieve_Image.m
+++ Tools/TCP Send Image/Example_TCP_Recieve_Image.m
@@ -10,7 +10,8 @@ catch
     error('Error: Please install the instrument control toolbox.');
 end
 
-t.InputBufferSize = 2^26;
+% setup the buffer size to be about 33MB
+t.InputBufferSize = 2^25;
 
 % wait for a connection
 fprintf(1, 'Waiting for connection...');
@@ -21,15 +22,30 @@ fprintf(1, 'CONNECTED\n');
 % read image size                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% wait for data, we know labview is going to send an array of type int32
-% with three values (rows, columns, num frames) so we expect to get 12
-% bytes
+% wait for data, labview is going to send an array of type int32 with three
+% values (rows, columns, number of frames) so we expect to get 12 bytes
 while t.BytesAvailable ~= 12
     pause(0.01)
 end
 
 % read data image size
 imageSize = fread(t, [3, 1], 'int32');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% preallocate and set parameters before sending the ready signal          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% convert the image size to byes, the image will be 16 bit (2 bytes) so we
+% have the total number of pixels times two
+imByteSize = 2*prod(imageSize);
+
+% array to store image
+myImage = zeros(imByteSize, 1, 'uint8');
+
+% track total bytes received
+totalBytes = 0;
+
+fprintf(1, 'Downloading image...');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % send back number 1, ready for image                                     %
@@ -45,29 +61,76 @@ fwrite(t, sendData, 'char')
 % read image data                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% convert the image size to byes, the image will be 16 bit (2 bytes)
-imByteSize = 2*prod(imageSize);
-
-fprintf(1, 'Waiting for image...');
-% wait for data
-while t.BytesAvailable ~= imByteSize
-    fprintf(1, 'Bytes: %d\n', t.BytesAvailable);
-    pause(0.01)
+% use try / catch to ensure connection is closed even if there is an error
+try
+    
+    while totalBytes < imByteSize
+        
+        readBytes = t.BytesAvailable;
+        
+        if readBytes > 0
+            
+            % read image data
+            myImage(totalBytes + 1:totalBytes + readBytes, 1) = fread(t, readBytes, 'uint8');
+            
+            % update total bytes received
+            totalBytes = totalBytes + readBytes;
+            
+        end
+        
+    end
+    
+    fprintf(1, 'DONE\n');
+    
+    % convert the image to 16 bit, for some reason the 8 bit elements are
+    % in the reverse order expected by the typecast function so we flip
+    % them before conversion and then flip back
+    myImage = flip(myImage, 1);
+    myImage = typecast(myImage, 'uint16');
+    myImage = flip(myImage, 1);
+    
+    % fix the image size array to match the image
+    imageSize = [imageSize(3), imageSize(2), imageSize(1)];
+    
+    % reshape into an image stack
+    myImage = reshape(myImage, imageSize(1), imageSize(2), imageSize(3));
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % send back number 1 to indicate success                              %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % data to send
+    sendData = char('1');
+    
+    % send the data
+    fwrite(t, sendData, 'char')
+    
+catch MExc
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % send back number 0 to indicate failure                              %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % print error information
+    warning('Download Error.')
+    fprintf(1,'Error ID: %s\n', MExc.identifier);
+    fprintf(1,'Error Message: %s\n', MExc.message);
+    
+    % data to send
+    sendData = char('0');
+    
+    % send the data
+    fwrite(t, sendData, 'char')
+    
 end
-fprintf(1, 'DONE\n');
 
-% read image
-fprintf(1, 'Reading image...');
-myImage = fread(t, prod(imageSize), 'uint16');
-myImage = reshape(myImage, imageSize(3), imageSize(2), imageSize(1));
-fprintf(1, 'DONE\n');
+% close the connection
+fclose(t);
+
+clear curPixels imByteSize numPixels readBytes sendData totalBytes t
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% send back number 1, success image read                                  %
+% display the first frame                                                 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% data to send
-sendData = char('1');
-
-% send the data
-fwrite(t, sendData, 'char')
+imagesc(myImage(:, :, 1)); axis image; colormap gray
